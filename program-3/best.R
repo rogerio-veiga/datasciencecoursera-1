f0368e0ddc8ba1c47ee7ebcee292d7062e73ec82
Todd Kemmerling <todd.kemmerling@tkemm-mb13r.corp.creditkarma.com>
2016-04-03 23:19:42 -0400

    Sync Point

--- program-3/best.R
+++ program-3/best.R
@@ -14,10 +14,46 @@ best <- function(state, outcome) {
   {
     stop("invalid outcome")
   }
+  okay <- FALSE
+  state_codes <- read.csv('state_codes.csv')
+  for ( s in state_codes$StateCode )
+  {
+    if (s == state)
+    {
+      okay <- TRUE
+      break
+    }
+  }
+  if (okay == FALSE)
+  {
+    stop("invalid state")
+  }
   data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-  data[, 11] <- as.numeric(data[, 11])
+  data[, 11] <- as.numeric(data[, 11]) ## heart attach
+  data[, 17] <- as.numeric(data[, 17]) ## heart failure
+  data[, 23] <- as.numeric(data[, 23]) ## pneumonia
   by_state_data = data[ data$State == state,]
-  order_by_data_data = by_state_data[order(by_state_data[11]),]
+  idx <- 0
+  if (outcome == "heart attack")
+  {
+    idx <- 11
+  }
+  else
+  {
+    if ( outcome == "heart failure")
+    {
+      idx <- 17
+    }
+    else
+    {
+      idx <- 23
+    }
+  }
+  if (idx == 0)
+  {
+    stop("Something Went Wrong")
+  }
+  order_by_data_data = by_state_data[order(by_state_data[idx],by_state_data[2]),]
   order_by_data_data[1,2]
 }
 ## Check that state and outcome are valid
