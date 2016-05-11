d76bee4955eccf9eff0ba337bfe4e3b87c168271
Todd Kemmerling <tkemmerling@yahoo.com>
2016-05-10 20:59:54 -0700

    Plot Three Working Again

--- proj-4/plot3.R
+++ proj-4/plot3.R
@@ -1,3 +1,4 @@
+library(ggplot2)
 library(dplyr)
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
@@ -8,3 +9,14 @@ NEI$type <- factor(NEI$type)
 df <- NEI %>% group_by(year,type) %>% summarise(mean(Emissions),min(Emissions), max(Emissions), median(Emissions))
 
 colnames(df) <- make.names(colnames(df))
+
+p <- ggplot(df, aes(x=year, y=mean.Emissions., colour=type)) +
+  geom_point(alpha=.3) +
+  geom_smooth(alpha=.2, size=1) +
+  ggtitle("Mean Emissions Vs Time") +
+  xlab('Mean Emissions') +
+  ylab('Year')
+  
+png('plot3.png')
+print(p)
+dev.off()
