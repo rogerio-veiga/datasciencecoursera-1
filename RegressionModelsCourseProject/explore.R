917963c5b266fae0d2be623a2ca0583257757e7e
Todd Kemmerling <tkemmerling@yahoo.com>
2016-08-28 18:04:30 -0700

    Initial checking of files.

---
+++ RegressionModelsCourseProject/explore.R
@@ -0,0 +1,16 @@
+# Explore The Data
+
+# Also, oddly, 0 means a V-engine, and 1 straight engine.
+# Oddlier, am = 0 is automatic and 1 is manual. 
+# [, 1]	mpg	Miles/(US) gallon
+# [, 2]	cyl	Number of cylinders
+# [, 3]	disp	Displacement (cu.in.)
+# [, 4]	hp	Gross horsepower
+# [, 5]	drat	Rear axle ratio
+# [, 6]	wt	Weight (1000 lbs)
+# [, 7]	qsec	1/4 mile time
+# [, 8]	vs	V/S
+# [, 9]	am	Transmission (0 = automatic, 1 = manual)
+# [,10]	gear	Number of forward gears
+# [,11]	carb	Number of carburetors
+
